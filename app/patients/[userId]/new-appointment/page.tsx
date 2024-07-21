241046bf9b31d25bcb607dc91c48a7ff10b167c1
farhankeriwala <114573891+farhankeriwala@users.noreply.github.com>
2024-07-21 14:20:46 +0100

    First Commit

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { PatientForm } from "@/components/forms/PatientForm";
+import { AppointmentForm } from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+import * as Sentry from '@sentry/nextjs'
+
+
+export default async function NewAppointment({
+  params: { userId },
+}: SearchParamProps) {
+  const patient = await getPatient(userId);
+  Sentry.metrics.set("user_view_new-appointment", patient.name);
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src={"/assets/icons/logo-full.svg"}
+            width={1000}
+            height={1000}
+            alt="logo"
+            className="mb-12 h-10 w-fit"
+          />
+          <AppointmentForm
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
+          <p className="copyright mt-10 py-12">
+            © 2024 CarePulse
+          </p>
+        </div>
+      </section>
+      <Image
+        src={"/assets/images/appointment-img.png"}
+        width={1000}
+        height={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
